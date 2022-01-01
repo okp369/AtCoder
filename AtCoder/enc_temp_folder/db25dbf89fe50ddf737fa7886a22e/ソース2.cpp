4185b445b3b86c2b33b3e6c786f3ed0347f54c37
DESKTOP-8EIQ5OC\nojsvt <nojsvt@gmail.com>
2022-01-01 22:33:21 +0900

    https://atcoder.jp/contests/abc104/tasks/abc104_c

---
+++ AtCoder/enc_temp_folder/db25dbf89fe50ddf737fa7886a22e/ソース2.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+//-----------------------------------------------------------------
+int main() {
+    int D, G, p[11], c[11];
+    cin >> D >> G;
+    for (int i = 0; i < D; ++i) {
+        cin >> p[i] >> c[i];
+    }
+
+    int ans = 1e9;
+    for (int i = 0; i < (1 << D); ++i) {
+        int s = 0, num = 0, rest_max = -1;
+        for (int j = 0; j < D; ++j) {
+            if (i & (1 << j)) {
+                s += 100 * (j + 1) * p[j] + c[j];
+                num += p[j];
+            }
+            else {
+                rest_max = j;
+            }
+        } 
+        if (s < G) {
+            int s1 = 100 * (rest_max + 1); // ‰ğ‚¢‚Ä‚È‚¢•û‚Í1–â‰½“_H
+            int need = (G - s + s1 - 1) / s1;
+            if (need >= p[rest_max]) {
+                continue;
+            }
+            num += need;
+        }
+        ans = min(ans, num);
+    }
+    cout << ans << endl;
+}
---
+++ AtCoder/ソース2.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+//-----------------------------------------------------------------
+int main() {
+    int D, G, p[11], c[11];
+    cin >> D >> G;
+    for (int i = 0; i < D; ++i) {
+        cin >> p[i] >> c[i];
+    }
+
+    int ans = 1e9;
+    for (int i = 0; i < (1 << D); ++i) {
+        int s = 0, num = 0, rest_max = -1;
+        for (int j = 0; j < D; ++j) {
+            if (i & (1 << j)) {
+                s += 100 * (j + 1) * p[j] + c[j];
+                num += p[j];
+            }
+            else {
+                rest_max = j;
+            }
+        } 
+        if (s < G) {
+            int s1 = 100 * (rest_max + 1); // 解いてない方は1問何点？
+            int need = (G - s + s1 - 1) / s1; //　解いてない方をあと難問解く必要がある？
+            if (need >= p[rest_max]) {
+                continue;
+            }
+            num += need;
+        }
+        ans = min(ans, num);
+    }
+    cout << ans << endl;
+}
