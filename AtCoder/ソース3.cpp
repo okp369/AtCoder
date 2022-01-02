9f0dae36dcd88edadd6f6cac376cc17bdea0c61b
DESKTOP-8EIQ5OC\nojsvt <nojsvt@gmail.com>
2022-01-02 21:53:39 +0900

    https://atcoder.jp/contests/abc233/tasks/abc233_c

--- AtCoder/ソース3.cpp
+++ AtCoder/ソース3.cpp
@@ -1,57 +1,57 @@
-#include <iostream>
-#include <vector>
-
-using namespace std;
-
-int main() {
-	int N ;
-	int t1 = 0;
-	int t2 = 0;
-	cin >> N;
-	vector<int> t(N);
-	int ans = 1e9;
-
-	for (int i = 0; i < N; i++)
-	{
-		cin >> t[i];
-	}
-	// 2択が4回だから全部で16通り
-	// 0->t2 1->t1と考えると以下の16通り
-	/*0000
-    0001
-	0010
-	0011
-	0100
-	0101
-	0110
-	0111
-	1000
-	1001
-	1010
-	1011
-	1100
-	1101
-	1110
-	1111*/
-	// これらを全パターンbit全探索で検証していく
-
-	for (int i = 0; i < (1 << N); i++)
-	{
-		t1 = 0;
-		t2 = 0;
-		for (int j = 0; j < N; j++)
-		{
-			if (i & (1 << j))
-			{
-				t1 += t[j];
-			}
-			else 
-			{
-				t2 += t[j];
-			}
-		}
-		ans = min(ans, max(t1, t2));
-	}
-
-	cout << ans << endl;
-}
+//#include <iostream>
+//#include <vector>
+//
+//using namespace std;
+//
+//int main() {
+//	int N ;
+//	int t1 = 0;
+//	int t2 = 0;
+//	cin >> N;
+//	vector<int> t(N);
+//	int ans = 1e9;
+//
+//	for (int i = 0; i < N; i++)
+//	{
+//		cin >> t[i];
+//	}
+//	// 2択が4回だから全部で16通り
+//	// 0->t2 1->t1と考えると以下の16通り
+//	/*0000
+//    0001
+//	0010
+//	0011
+//	0100
+//	0101
+//	0110
+//	0111
+//	1000
+//	1001
+//	1010
+//	1011
+//	1100
+//	1101
+//	1110
+//	1111*/
+//	// これらを全パターンbit全探索で検証していく
+//
+//	for (int i = 0; i < (1 << N); i++)
+//	{
+//		t1 = 0;
+//		t2 = 0;
+//		for (int j = 0; j < N; j++)
+//		{
+//			if (i & (1 << j))
+//			{
+//				t1 += t[j];
+//			}
+//			else 
+//			{
+//				t2 += t[j];
+//			}
+//		}
+//		ans = min(ans, max(t1, t2));
+//	}
+//
+//	cout << ans << endl;
+//}
---
+++ AtCoder/ソース4.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<vector<long long>> a;
+long long ans = 0;
+long long N, X;
+
+void dfs(long long index, long long seki) {
+	if (index >= N)
+	{
+		if (seki == X) {
+			++ans;
+		}
+		return;
+	}
+
+	for (long long val : a[index]) {
+
+		dfs(index + 1, seki * val);
+	}
+}
+
+int main() {
+	cin >> N >> X;
+
+	a.resize(N);
+
+	for (long long i = 0; i < N; i++)
+	{
+		long long L;
+		cin >> L;
+
+		a[i].resize(L);
+		
+		for (long long j = 0; j < L; j++)
+		{
+			cin >> a[i][j];
+		}
+	}
+
+	dfs(0, 1);
+
+	cout << ans << endl;
+}
