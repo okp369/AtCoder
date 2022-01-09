54973d24fd012577d7e6262ec349e93fbe948f87
DESKTOP-8EIQ5OC\nojsvt <nojsvt@gmail.com>
2022-01-09 15:38:36 +0900

    https://atcoder.jp/contests/abc234/tasks/abc234_c

---
+++ AtCoder/ソース18.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+using namespace std;
+typedef long long ll;
+
+string convert(long long x) {
+    string res;
+    while (x > 0) {
+        res.push_back('0' + (x % 2));
+        x /= 2;
+    }
+    reverse(res.begin(), res.end());
+    return res;
+}
+void output(string s) {
+    for (auto& nx : s) {
+        if (nx == '1') { cout << '2'; }
+        else { cout << '0'; }
+    }
+    cout << '\n';
+}
+int main() {
+    long long k;
+    cin >> k;
+    output(convert(k));
+    return 0;
+}
