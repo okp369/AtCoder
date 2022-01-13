30a312bd2f7d8abb02a31187f392a7eac45533bc
DESKTOP-8EIQ5OC\nojsvt <nojsvt@gmail.com>
2022-01-13 22:54:05 +0900

    https://atcoder.jp/contests/abc216/tasks/abc216_c

--- AtCoder/ソース21.cpp
+++ AtCoder/ソース21.cpp
@@ -1,29 +1,29 @@
-#include <iostream>
-#include <vector>
-#include <string>
-#include <algorithm>
-using namespace std;
-typedef long long ll;
-
-int main() {
-    //sort(a.begin(), a.end());
-    //vector<vector<int>> A(N, vector<int>(2));
-    int N, P;
-    cin >> N >> P;
-    vector<int> a(N);
-    for (int i = 0; i < N; i++)
-    {
-        cin >> a[i];
-    }
-
-    int cnt = 0;
-    for (int i = 0; i < N; i++)
-    {
-        if (a[i] >= P)
-        {
-            ++cnt;
-        }
-    }
-
-    cout << cnt << endl;
-}
+//#include <iostream>
+//#include <vector>
+//#include <string>
+//#include <algorithm>
+//using namespace std;
+//typedef long long ll;
+//
+//int main() {
+//    //sort(a.begin(), a.end());
+//    //vector<vector<int>> A(N, vector<int>(2));
+//    int N, P;
+//    cin >> N >> P;
+//    vector<int> a(N);
+//    for (int i = 0; i < N; i++)
+//    {
+//        cin >> a[i];
+//    }
+//
+//    int cnt = 0;
+//    for (int i = 0; i < N; i++)
+//    {
+//        if (a[i] < P)
+//        {
+//            ++cnt;
+//        }
+//    }
+//
+//    cout << cnt << endl;
+//}
