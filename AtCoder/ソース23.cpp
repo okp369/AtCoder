60801a3520ae5012ee300ce68635bc609fac09b2
DESKTOP-8EIQ5OC\nojsvt <nojsvt@gmail.com>
2022-01-15 13:47:57 +0900

    https://atcoder.jp/contests/abc223/tasks/abc223_a

--- AtCoder/ソース23.cpp
+++ AtCoder/ソース23.cpp
@@ -1,26 +1,26 @@
-#include <iostream>
-#include <vector>
-#include <string>
-#include <algorithm>
-using namespace std;
-typedef long long ll;
-
-int main() {
-    //sort(a.begin(), a.end());
-    //vector<vector<int>> A(N, vector<int>(2));
-    
-    string SSS, a, b, c;
-    cin >> SSS;
-
-    a = SSS[0];
-    b = SSS[1];
-    c = SSS[2];
-
-    if (a == b && b == c)
-    {
-        cout << "Won" << endl;
-    }
-    else {
-        cout << "Lost" << endl;
-    }
-}
+//#include <iostream>
+//#include <vector>
+//#include <string>
+//#include <algorithm>
+//using namespace std;
+//typedef long long ll;
+//
+//int main() {
+//    //sort(a.begin(), a.end());
+//    //vector<vector<int>> A(N, vector<int>(2));
+//    
+//    string SSS, a, b, c;
+//    cin >> SSS;
+//
+//    a = SSS[0];
+//    b = SSS[1];
+//    c = SSS[2];
+//
+//    if (a == b && b == c)
+//    {
+//        cout << "Won" << endl;
+//    }
+//    else {
+//        cout << "Lost" << endl;
+//    }
+//}
---
+++ AtCoder/ソース24.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+using namespace std;
+typedef long long ll;
+
+int l;
+string s, t;
+vector<string> res;
+void find(int fl) {
+    if (fl == 0) {
+        res.push_back(t);
+        return;
+    }
+    for (int i = 0; i < l; i++) {
+        if (fl & (1 << i)) {
+            t.push_back(s[i]);
+            find(fl ^ (1 << i));
+            t.pop_back();
+        }
+    }
+}
+
+int main() {
+    int k;
+    cin >> s >> k;
+    l = s.size();
+    find((1 << l) - 1);
+    sort(res.begin(), res.end());
+    unique(res.begin(), res.end());
+    cout << res[k - 1] << '\n';
+    return 0;
+}
