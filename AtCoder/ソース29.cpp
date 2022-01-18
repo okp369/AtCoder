329583b6cb32f377bf8a8a89f6915c633fde1b2f
DESKTOP-8EIQ5OC\nojsvt <nojsvt@gmail.com>
2022-01-18 21:32:58 +0900

    https://atcoder.jp/contests/abc085/tasks/abc085_b

--- AtCoder/ソース29.cpp
+++ AtCoder/ソース29.cpp
@@ -1,40 +1,40 @@
-#include <iostream>
-#include <vector>
-#include <string>
-#include <algorithm>
-#include <map>
-using namespace std;
-typedef long long ll;
-#define rep(i,n) for (int i = 0; i < (n); ++i)
-
-int main() {
-    //sort(a.begin(), a.end());
-    //vector<vector<int>> A(N, vector<int>(2));
-    //map<int, vector<int>> mp;  //mp[a].push_back(i+1);
-    int n, q;
-    cin >> n >> q;
-    map<int, vector<int>> mp;
-    rep(i, n)
-    {
-        int a;
-        cin >> a;
-        mp[a].push_back(i+1);
-    }
-
-    rep(qi, q)
-    {
-        int x, k;
-        cin >> x >> k;
-        if (mp[x].size() < k)
-        {
-            cout << -1 << endl;
-        }
-        else
-        {
-            cout << mp[x][k - 1] << endl;
-        }
-        /*vector<int>& p = mp[x];
-        if (p.size() < k) cout << -1 << endl;
-        else cout << p[k - 1] << endl;*/
-    }
-}
+//#include <iostream>
+//#include <vector>
+//#include <string>
+//#include <algorithm>
+//#include <map>
+//using namespace std;
+//typedef long long ll;
+//#define rep(i,n) for (int i = 0; i < (n); ++i)
+//
+//int main() {
+//    //sort(a.begin(), a.end());
+//    //vector<vector<int>> A(N, vector<int>(2));
+//    //map<int, vector<int>> mp;  //mp[a].push_back(i+1);
+//    int n, q;
+//    cin >> n >> q;
+//    map<int, vector<int>> mp;
+//    rep(i, n)
+//    {
+//        int a;
+//        cin >> a;
+//        mp[a].push_back(i+1);
+//    }
+//
+//    rep(qi, q)
+//    {
+//        int x, k;
+//        cin >> x >> k;
+//        if (mp[x].size() < k)
+//        {
+//            cout << -1 << endl;
+//        }
+//        else
+//        {
+//            cout << mp[x][k - 1] << endl;
+//        }
+//        /*vector<int>& p = mp[x];
+//        if (p.size() < k) cout << -1 << endl;
+//        else cout << p[k - 1] << endl;*/
+//    }
+//}
