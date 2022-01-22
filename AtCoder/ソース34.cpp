5e3406d83a55618eebdcf4531c338de4807f8f5e
DESKTOP-8EIQ5OC\nojsvt <nojsvt@gmail.com>
2022-01-22 11:45:50 +0900

    https://atcoder.jp/contests/abc217/tasks/abc217_b

---
+++ AtCoder/ソース34.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <map>
+using namespace std;
+typedef long long ll;
+#define rep(i,n) for (int i = 0; i < (n); ++i)
+bool agc, abc, arc, ahc;
+void check(string str)
+{
+    if (str == "AGC")
+    {
+        agc = true;
+    }
+    else if (str == "ABC")
+    {
+        abc = true;
+    }
+    else if (str == "ARC")
+    {
+        arc = true;
+    }
+    else if (str == "AHC")
+    {
+        ahc = true;
+    }
+}
+int main() {
+    //sort(a.begin(), a.end());
+    //vector<vector<int>> A(N, vector<int>(2));
+        //map<int, vector<int>> mp;  ->  mp[a].push_back(i+1);
+    //map<string, int> s; s[title] += 1; for (pair<string,int> val : s){} cnt = max(cnt, val.second);
+    string a, b, c;
+    cin >> a >> b >> c;
+    check(a);
+    check(b);
+    check(c);
+    
+    if (!agc)
+    {
+        cout << "AGC" << endl;
+    }
+    else if (!abc)
+    {
+        cout << "ABC" << endl;
+    }
+    else if (!arc)
+    {
+        cout << "ARC" << endl;
+    }
+    else
+    {
+        cout << "AHC" << endl;
+    }
+}
+
