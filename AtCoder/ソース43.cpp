1d5fafac57dcc4fe1876ea45ca04707d337563bf
DESKTOP-8EIQ5OC\nojsvt <nojsvt@gmail.com>
2022-01-26 22:56:21 +0900

    https://atcoder.jp/contests/abc236/tasks/abc236_c

--- AtCoder/ソース43.cpp
+++ AtCoder/ソース43.cpp
@@ -7,42 +7,24 @@
 //using namespace std;
 //typedef long long ll;
 //#define rep(i,n) for (int i = 0; i < (n); ++i)
+//map<string, int> mp;
+//string s[100010];
 //
 //int main() {
-//    ll n, m;
-//    cin >> n >> m;
-//    vector<string> s(n);
-//    vector<string> t(m);
-//    map<string, char> mp;
-//
-//    for (ll i = 0; i < n; i++)
-//    {
-//        cin >> s[i];
-//        mp[s[i]];
-//    }
-//
-//    for (ll i = 0; i < m; i++)
-//    {
-//        cin >> t[i];
-//        auto a = mp.find(t[i]);
-//        auto b = mp.end();
-//        if (mp.find(t[i]) != mp.end())
-//        {
-//            mp[t[i]] = 1;
-//        }
-//    }
-//
-//    for (ll i = 0; i < n; i++)
-//    {
-//        if (mp[s[i]] == 1)
-//        {
-//            cout << "Yes" << endl;
-//        }
-//        else
-//        {
-//            cout << "No" << endl;
-//        }
-//    }
-//
-//    return 0;
+//	int n, m;
+//	cin >> n >> m;
+//	for (int i = 1; i <= n; i++) cin >> s[i];
+//	for (int i = 1; i <= m; i++)
+//	{
+//		string t;
+//		cin >> t;
+//		mp[t] = 1;
+//	}
+//	for (int i = 1; i <= n; i++)
+//	{
+//		if (mp.find(s[i]) != mp.end())
+//			cout << "Yes" << endl;
+//		else cout << "No" << endl;
+//	}
+//	return 0;
 //}
---
+++ AtCoder/ソース45.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <map>
+#include <set>
+using namespace std;
+typedef long long ll;
+#define rep(i,n) for (int i = 0; i < (n); ++i)
+
+int main() {
+    ll n, m, ans = 0;
+    cin >> n >> m;
+    vector<string> s(n);
+    map<string, int> stop;
+    rep(i, n) {
+        cin >> s[i];
+    }
+
+    rep(i, m) {
+        string t;
+        cin >> t;
+        stop[t] = 1;
+    }
+
+    rep(i, n) {
+        if (stop.find(s[i]) != stop.end()) cout << "Yes" << endl;
+        else cout << "No" << endl;
+    }
+
+}
