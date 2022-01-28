4c518147205e8732d701fe01b4664e86c0f2e3a5
DESKTOP-8EIQ5OC\nojsvt <nojsvt@gmail.com>
2022-01-28 22:05:01 +0900

    https://atcoder.jp/contests/abc218/tasks/abc218_a

--- AtCoder/ソース46.cpp
+++ AtCoder/ソース46.cpp
@@ -1,43 +1,43 @@
-#include <iostream>
-#include <vector>
-#include <string>
-#include <algorithm>
-#include <map>
-#include <set>
-using namespace std;
-typedef long long ll;
-#define rep(i,n) for (int i = 0; i < (n); ++i)
-
-int main() {
-    //sort(a.begin(), a.end());
-    //vector<vector<int>> A(N, vector<int>(2));
-        //map<int, vector<int>> mp;  ->  mp[a].push_back(i+1);
-    //map<string, int> s; s[title] += 1; for (pair<string,int> val : s){} cnt = max(cnt, val.second);
-    int n, m;
-    cin >> n >> m;
-    vector<string> s(n);
-    map<string ,bool> mp;
-    rep(i, n)
-    {
-        cin >> s[i];
-    }
-
-    rep(i, m)
-    {
-        string mm;
-        cin >> mm;
-        mp[mm] = true;
-    }
-
-    rep(i, n)
-    {
-        if (mp[s[i]])
-        {
-            cout << "Yes" << endl;
-        }
-        else
-        {
-            cout << "No" << endl;
-        }
-    }
-}
+//#include <iostream>
+//#include <vector>
+//#include <string>
+//#include <algorithm>
+//#include <map>
+//#include <set>
+//using namespace std;
+//typedef long long ll;
+//#define rep(i,n) for (int i = 0; i < (n); ++i)
+//
+//int main() {
+//    //sort(a.begin(), a.end());
+//    //vector<vector<int>> A(N, vector<int>(2));
+//        //map<int, vector<int>> mp;  ->  mp[a].push_back(i+1);
+//    //map<string, int> s; s[title] += 1; for (pair<string,int> val : s){} cnt = max(cnt, val.second);
+//    int n, m;
+//    cin >> n >> m;
+//    vector<string> s(n);
+//    map<string ,bool> mp;
+//    rep(i, n)
+//    {
+//        cin >> s[i];
+//    }
+//
+//    rep(i, m)
+//    {
+//        string mm;
+//        cin >> mm;
+//        mp[mm] = true;
+//    }
+//
+//    rep(i, n)
+//    {
+//        if (mp[s[i]])
+//        {
+//            cout << "Yes" << endl;
+//        }
+//        else
+//        {
+//            cout << "No" << endl;
+//        }
+//    }
+//}
