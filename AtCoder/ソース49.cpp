9a4482e8a036d46a3b65582b8f5b4803c43aa5f2
DESKTOP-8EIQ5OC\nojsvt <nojsvt@gmail.com>
2022-01-31 23:59:40 +0900

    no message

---
+++ AtCoder/ソース49.cpp
@@ -0,0 +1,40 @@
+//#include <iostream>
+//#include <vector>
+//#include <string>
+//#include <algorithm>
+//#include <map>
+//#include <set>
+//using namespace std;
+//typedef long long ll;
+//#define rep(i,n) for (int i = 0; i < (n); ++i)
+//
+//int main() {
+//    //sort(a.begin(), a.end());
+//    //vector<vector<int>> A(N, vector<int>(2));
+//        //map<int, vector<int>> mp;  ->  mp[a].push_back(i+1);
+//    //map<string, int> s; s[title] += 1; for (pair<string,int> val : s){} cnt = max(cnt, val.second);
+//    string s, copyS;
+//    cin >> s;
+//
+//    while (s[s.size()-1] == 'a') {
+//        if (s.size() <= 1)
+//        {
+//            break;
+//        }
+//        else {
+//            s.erase(s.size() - 1, s.size());
+//        } 
+//    }
+//
+//    copyS = s;
+//    reverse(copyS.begin(), copyS.end());
+//
+//    if (s == copyS)
+//    {
+//        cout << "Yes" << endl;
+//    }
+//    else
+//    {
+//        cout << "No" << endl;
+//    }  
+//}
