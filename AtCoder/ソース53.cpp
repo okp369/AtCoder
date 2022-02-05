eddfe4b913d888908b5d5febbd543897b62aa39a
DESKTOP-8EIQ5OC\nojsvt <nojsvt@gmail.com>
2022-02-05 22:41:40 +0900

    https://atcoder.jp/contests/abc238/tasks/abc238_a

--- AtCoder/ソース53.cpp
+++ AtCoder/ソース53.cpp
@@ -1,26 +1,55 @@
-#include <iostream>
-#include <vector>
-#include <string>
-#include <algorithm>
-#include <map>
-#include <set>
-using namespace std;
-typedef long long ll;
-#define rep(i,n) for (int i = 0; i < (n); ++i)
-
-int main() {
-    string s;
-    cin >> s;
-    int n = s.size();
-    int l = 0, r = n - 1;
-    while (l < r && s[l] == 'a' && s[r] == 'a') {
-        ++l; --r;
-    }
-    while (l < r && s[r] == 'a') --r;
-    while (l < r && s[l] == s[r]) {
-        ++l; --r;
-    }
-    if (l >= r) cout << "Yes" << endl;
-    else cout << "No" << endl;
-    return 0;
-}
+//#include <iostream>
+//#include <vector>
+//#include <string>
+//#include <algorithm>
+//#include <map>
+//#include <set>
+//using namespace std;
+//typedef long long ll;
+//#define rep(i,n) for (int i = 0; i < (n); ++i)
+//
+//int main() {
+//    //sort(a.begin(), a.end());
+//    //vector<vector<int>> A(N, vector<int>(2));
+//        //map<int, vector<int>> mp;  ->  mp[a].push_back(i+1);
+//    //map<string, int> s; s[title] += 1; for (pair<string,int> val : s){} cnt = max(cnt, val.second);
+//    string s, copyS;
+//    cin >> s;
+//
+//    ll saigo = s.size()-1;
+//
+//    // –`“ª‰½ŒÂ–Ú‚Ü‚Åa‚ª‘±‚­‚©
+//    ll boutou_a = -1;
+//    ll saigo_a = -1;
+//    bool close = false;
+//    for (ll i = 0; i < s.size()-1; i++)
+//    {
+//        if (s[saigo-i] == 'a') {
+//            saigo_a = saigo - i;
+//            if (s[i] == 'a' && !close) {
+//                boutou_a = i;
+//            }
+//            else {
+//                close = true;
+//            }
+//        }
+//        else {
+//            break;
+//        }
+//
+//    }
+//
+//    string x = s.substr(boutou_a+1, saigo_a-1);
+//
+//    ll len = s.size();
+//    
+//
+//    //if (s == copyS)
+//    //{
+//    //    cout << "Yes" << endl;
+//    //}
+//    //else
+//    //{
+//    //    cout << "No" << endl;
+//    //}
+//}
---
+++ AtCoder/ソース54.cpp
@@ -0,0 +1,21 @@
+//#include <iostream>
+//#include <vector>
+//#include <string>
+//#include <algorithm>
+//#include <map>
+//#include <set>
+//using namespace std;
+//typedef long long ll;
+//#define rep(i,n) for (int i = 0; i < (n); ++i)
+//
+//int main() {
+//    //sort(a.begin(), a.end());
+//    //vector<vector<int>> A(N, vector<int>(2));
+//        //map<int, vector<int>> mp;  ->  mp[a].push_back(i+1);
+//    //map<string, int> s; s[title] += 1; for (pair<string,int> val : s){} cnt = max(cnt, val.second);
+//    ll n;
+//    cin >> n;
+//    
+//    if (n == 2 || n == 3 || n == 4)cout << "No" << endl;
+//    else cout << "Yes" << endl;
+//}
