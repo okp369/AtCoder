1a0b06fa0462adc387de36335a49c4d386ff1fb7
DESKTOP-8EIQ5OC\nojsvt <nojsvt@gmail.com>
2022-02-08 21:33:43 +0900

    https://atcoder.jp/contests/abc208/tasks/abc208_c

--- AtCoder/ソース57.cpp
+++ AtCoder/ソース57.cpp
@@ -1,45 +1,46 @@
-#include <iostream>
-#include <vector>
-#include <string>
-#include <algorithm>
-#include <map>
-#include <set>
-using namespace std;
-typedef long long ll;
-#define rep(i,n) for (int i = 0; i < (n); ++i)
-
-const int MaxA = 400;
-
-int main() {
-    //sort(a.begin(), a.end());
-    //vector<vector<int>> A(N, vector<int>(2));
-        //map<int, vector<int>> mp;  ->  mp[a].push_back(i+1);
-    //map<string, int> s; s[title] += 1; for (pair<string,int> val : s){} cnt = max(cnt, val.second);
-    int n;
-    cin >> n;
-    vector<int> a(n);
-    rep(i, n) {
-        int xx;
-        cin >> xx;
-        xx += 200;
-        a[i] = xx;
-    }
-    vector<int> exist(401);
-    
-    ll ans = 0;
-
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j <= MaxA; j++) // 0 `@400 a[i]‚ª‚Æ‚è‚¤‚é”
-        {
-            if (exist[j] != 0) // ‰ß‹Ž‚Éj‚Æ‚¢‚¤”‚ª‚ ‚Á‚½ê‡A·‚Ì2æ‚ðans‚É‘«‚³‚È‚¢‚Æ‚¢‚¯‚È‚¢
-            {
-                ans += (a[i] - j) * (a[i] - j) * exist[j];
-            }
-        }
-        ++exist[a[i]];
-    }
-
-    cout << ans << endl;
-    return 0;
-}
+//#include <iostream>
+//#include <vector>
+//#include <string>
+//#include <algorithm>
+//#include <map>
+//#include <set>
+//using namespace std;
+//typedef long long ll;
+//#define rep(i,n) for (int i = 0; i < (n); ++i)
+//
+//const int MaxA = 400;
+//
+//int main() {
+//    //sort(a.begin(), a.end());
+//    //vector<vector<int>> A(N, vector<int>(2));
+//        //map<int, vector<int>> mp;  ->  mp[a].push_back(i+1);
+//    //map<string, int> s; s[title] += 1; for (pair<string,int> val : s){} cnt = max(cnt, val.second);
+//    int n;
+//    cin >> n;
+//    vector<int> a(n);
+//    rep(i, n) {
+//        int xx;
+//        cin >> xx;
+//        xx += 200;
+//        a[i] = xx;
+//    }
+//    vector<ll> exist(401);
+//    
+//    ll ans = 0;
+//
+//    for (int i = 0; i < n; i++)
+//    {
+//        for (int j = 0; j <= MaxA; j++) // 0 `@400 a[i]‚ª‚Æ‚è‚¤‚é”
+//        {
+//            if (exist[j] != 0) // ‰ß‹Ž‚Éj‚Æ‚¢‚¤”‚ª‚ ‚Á‚½ê‡A·‚Ì2æ‚ðans‚É‘«‚³‚È‚¢‚Æ‚¢‚¯‚È‚¢
+//            {
+//                int sa = a[i] - j;
+//                ans += sa * sa * exist[j];
+//            }
+//        }
+//        ++exist[a[i]];
+//    }
+//
+//    cout << ans << endl;
+//    return 0;
+//}
