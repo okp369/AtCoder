e941298ebece8bcc0b16cbb48c49c734b0942a77
DESKTOP-8EIQ5OC\nojsvt <nojsvt@gmail.com>
2022-02-16 23:33:38 +0900

    https://atcoder.jp/contests/abc186/tasks/abc186_a

--- AtCoder/ソース62.cpp
+++ AtCoder/ソース62.cpp
@@ -13,8 +13,8 @@ int main() {
     //vector<vector<int>> A(N, vector<int>(2));
         //map<int, vector<int>> mp;  ->  mp[a].push_back(i+1);
     //map<string, int> s; s[title] += 1; for (pair<string,int> val : s){} cnt = max(cnt, val.second);
-    int n;
-    cin >> n;
-    vector<int> a(n);
-    rep(i, n) cin >> a[i];
+    int n, m;
+    cin >> n >> m;
+   
+    cout << n / m << endl;
 }
--- AtCoder/ソース65.cpp
+++ AtCoder/ソース65.cpp
@@ -1,31 +1,31 @@
-#include <iostream>
-#include <vector>
-#include <string>
-#include <algorithm>
-#include <map>
-#include <set>
-using namespace std;
-typedef long long ll;
-#define rep(i,n) for (int i = 0; i < (n); ++i)
-
-int main() {
-    //sort(a.begin(), a.end());
-    //vector<vector<int>> A(N, vector<int>(2));
-        //map<int, vector<int>> mp;  ->  mp[a].push_back(i+1);
-    //map<string, int> s; s[title] += 1; for (pair<string,int> val : s){} cnt = max(cnt, val.second);
-    int n, m;
-    cin >> n >> m;
-    vector<int> a(n);
-    rep(i, n) cin >> a[i];
-
-    sort(a.begin(), a.end());
-
-    rep(i, m)
-    {
-        int x;
-        cin >> x;
-        int p = lower_bound(a.begin(), a.end(), x) - a.begin();
-        if (p >= n) cout << 0 << endl;
-        else cout << n - p << endl;
-    }
-}
+//#include <iostream>
+//#include <vector>
+//#include <string>
+//#include <algorithm>
+//#include <map>
+//#include <set>
+//using namespace std;
+//typedef long long ll;
+//#define rep(i,n) for (int i = 0; i < (n); ++i)
+//
+//int main() {
+//    //sort(a.begin(), a.end());
+//    //vector<vector<int>> A(N, vector<int>(2));
+//        //map<int, vector<int>> mp;  ->  mp[a].push_back(i+1);
+//    //map<string, int> s; s[title] += 1; for (pair<string,int> val : s){} cnt = max(cnt, val.second);
+//    int n, m;
+//    cin >> n >> m;
+//    vector<int> a(n);
+//    rep(i, n) cin >> a[i];
+//
+//    sort(a.begin(), a.end());
+//
+//    rep(i, m)
+//    {
+//        int x;
+//        cin >> x;
+//        int p = lower_bound(a.begin(), a.end(), x) - a.begin();
+//        if (p >= n) cout << 0 << endl;
+//        else cout << n - p << endl;
+//    }
+//}
