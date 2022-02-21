bdffad7193ab4188e9b118953a0cb50da24632ca
DESKTOP-8EIQ5OC\nojsvt <nojsvt@gmail.com>
2022-02-21 23:23:55 +0900

    https://atcoder.jp/contests/abc239/tasks/abc239_b

--- AtCoder/ソース68.cpp
+++ AtCoder/ソース68.cpp
@@ -1,59 +1,59 @@
-#include <iostream>
-#include <vector>
-#include <string>
-#include <algorithm>
-#include <map>
-#include <set>
-using namespace std;
-typedef long long ll;
-#define rep(i,n) for (ll i = 0; i < (n); ++i)
-
-ll n, x;
-vector<vector<ll>> mp;
-ll ans = 0;
-
-void saiki(ll index, ll now)
-{
-    if (index >= n)
-    {
-        if (now == x)
-        {
-            ++ans;
-        }
-        return;
-    }
-
-    for (ll val : mp[index])
-    {
-        if (now * val > x) continue;
-        saiki(index + 1, now * val);
-    }
-}
-
-int main() {
-    //sort(a.begin(), a.end());
-    //vector<vector<int>> A(N, vector<int>(2));
-        //map<int, vector<int>> mp;  ->  mp[a].push_back(i+1);
-    //map<string, int> s; s[title] += 1; for (pair<string,int> val : s){} cnt = max(cnt, val.second);
-
-    cin >> n >> x;
-    
-    mp.resize(n);
-
-    rep(i, n)
-    {
-        ll len;
-        cin >> len;
-        mp[i].resize(len);
-        rep(j, len)
-        {
-            ll val;
-            cin >> val;
-            mp[i][j] = val;
-        }
-    }
-    
-    saiki(0, 1);
-
-    cout << ans << endl;
-}
+//#include <iostream>
+//#include <vector>
+//#include <string>
+//#include <algorithm>
+//#include <map>
+//#include <set>
+//using namespace std;
+//typedef long long ll;
+//#define rep(i,n) for (ll i = 0; i < (n); ++i)
+//
+//ll n, x;
+//vector<vector<ll>> mp;
+//ll ans = 0;
+//
+//void saiki(ll index, ll now)
+//{
+//    if (index >= n)
+//    {
+//        if (now == x)
+//        {
+//            ++ans;
+//        }
+//        return;
+//    }
+//
+//    for (ll val : mp[index])
+//    {
+//        if (now * val > x) continue;
+//        saiki(index + 1, now * val);
+//    }
+//}
+//
+//int main() {
+//    //sort(a.begin(), a.end());
+//    //vector<vector<int>> A(N, vector<int>(2));
+//        //map<int, vector<int>> mp;  ->  mp[a].push_back(i+1);
+//    //map<string, int> s; s[title] += 1; for (pair<string,int> val : s){} cnt = max(cnt, val.second);
+//
+//    cin >> n >> x;
+//    
+//    mp.resize(n);
+//
+//    rep(i, n)
+//    {
+//        ll len;
+//        cin >> len;
+//        mp[i].resize(len);
+//        rep(j, len)
+//        {
+//            ll val;
+//            cin >> val;
+//            mp[i][j] = val;
+//        }
+//    }
+//    
+//    saiki(0, 1);
+//
+//    cout << ans << endl;
+//}
---
+++ AtCoder/ソース69.cpp
@@ -0,0 +1,32 @@
+//#include <iostream>
+//#include <vector>
+//#include <string>
+//#include <algorithm>
+//#include <map>
+//#include <set>
+//#include <cstdio>
+//#include <iostream>
+//#include <iomanip>
+//using namespace std;
+//typedef long long ll;
+//#define rep(i,n) for (int i = 0; i < (n); ++i)
+//
+//int main() {
+//    //sort(a.begin(), a.end());
+//    //vector<vector<int>> A(N, vector<int>(2));
+//        //map<int, vector<int>> mp;  ->  mp[a].push_back(i+1);
+//    //map<string, int> s; s[title] += 1; for (pair<string,int> val : s){} cnt = max(cnt, val.second);
+//    double x;
+//    cin >> x;
+//
+//    double a = x * 12800000;
+//    double b = x * x;
+//
+//    double ans = a + b;
+//
+//    double xxx = sqrt(ans);
+//
+//    cout << fixed << setprecision(2) << xxx << endl; // C++
+//
+//    
+//}
