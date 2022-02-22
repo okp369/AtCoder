b7909ee142eb833bfb9d2c18c1a5081bc4c9b2b5
DESKTOP-8EIQ5OC\nojsvt <nojsvt@gmail.com>
2022-02-22 23:27:04 +0900

    https://atcoder.jp/contests/abc239/tasks/abc239_a

---
+++ AtCoder/ソース70.cpp
@@ -0,0 +1,31 @@
+//#include <iostream>
+//#include <vector>
+//#include <string>
+//#include <algorithm>
+//#include <map>
+//#include <set>
+//using namespace std;
+//typedef long long ll;
+//#define rep(i,n) for (int i = 0; i < (n); ++i)
+//
+//int main() {
+//    //sort(a.begin(), a.end());
+//    //vector<vector<int>> A(N, vector<int>(2));
+//        //map<int, vector<int>> mp;  ->  mp[a].push_back(i+1);
+//    //map<string, int> s; s[title] += 1; for (pair<string,int> val : s){} cnt = max(cnt, val.second);
+//    ll n;
+//    cin >> n;
+//    
+//    ll ans = n / 10;
+//    
+//
+//    if (n < 0 && n%10 != 0)
+//    {
+//        cout << ans - 1 << endl;
+//    }
+//    else {
+//        cout << ans << endl;
+//    }
+//    
+//    
+//}
