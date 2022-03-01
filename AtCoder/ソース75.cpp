4850b17a1bc2c0c1f727a0987e903cf5cbcc9fb1
DESKTOP-8EIQ5OC\nojsvt <nojsvt@gmail.com>
2022-03-01 22:59:09 +0900

    https://atcoder.jp/contests/abc241/tasks/abc241_b

--- AtCoder/ソース75.cpp
+++ AtCoder/ソース75.cpp
@@ -1,27 +1,27 @@
-#include <iostream>
-#include <vector>
-#include <string>
-#include <algorithm>
-#include <map>
-#include <set>
-using namespace std;
-typedef long long ll;
-#define rep(i,n) for (int i = 0; i < (n); ++i)
-
-int main() {
-    //sort(a.begin(), a.end());
-    //vector<vector<int>> A(N, vector<int>(2));
-        //map<int, vector<int>> mp;  ->  mp[a].push_back(i+1);
-    //map<string, int> s; s[title] += 1; for (pair<string,int> val : s){} cnt = max(cnt, val.second);
-    
-    vector<int> vec(10);
-    rep(i, 10)
-    {
-        int x;
-        cin >> x;
-        vec[i] = x;
-
-    }
-
-    cout << vec[vec[vec[0]]] << endl;
-}
+//#include <iostream>
+//#include <vector>
+//#include <string>
+//#include <algorithm>
+//#include <map>
+//#include <set>
+//using namespace std;
+//typedef long long ll;
+//#define rep(i,n) for (int i = 0; i < (n); ++i)
+//
+//int main() {
+//    //sort(a.begin(), a.end());
+//    //vector<vector<int>> A(N, vector<int>(2));
+//        //map<int, vector<int>> mp;  ->  mp[a].push_back(i+1);
+//    //map<string, int> s; s[title] += 1; for (pair<string,int> val : s){} cnt = max(cnt, val.second);
+//    
+//    vector<int> vec(10);
+//    rep(i, 10)
+//    {
+//        int x;
+//        cin >> x;
+//        vec[i] = x;
+//
+//    }
+//
+//    cout << vec[vec[vec[0]]] << endl;
+//}
---
+++ AtCoder/ソース76.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <map>
+#include <set>
+using namespace std;
+typedef long long ll;
+#define rep(i,n) for (int i = 0; i < (n); ++i)
+
+int main() {
+    //sort(a.begin(), a.end());
+    //vector<vector<int>> A(N, vector<int>(2));
+        //map<int, vector<int>> mp;  ->  mp[a].push_back(i+1);
+    //map<string, int> s; s[title] += 1; for (pair<string,int> val : s){} cnt = max(cnt, val.second);
+    int n, m;
+    cin >> n >> m;
+    
+    map<int, int> mp;
+
+    rep(i, n) {
+        int x;
+        cin >> x;
+        mp[x]++;
+    }
+
+    bool a = false;
+    rep(i, m) {
+        int x;
+        cin >> x;
+        mp[x]--;
+        if (mp[x] < 0) a = true;
+    }
+
+    if (a) cout << "No" << endl;
+    else cout << "Yes" << endl;
+} 
