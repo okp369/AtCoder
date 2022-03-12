3c0722af70ec09d1ac9facbfb9f8a3db214c8e22
DESKTOP-8EIQ5OC\nojsvt <nojsvt@gmail.com>
2022-03-12 15:43:28 +0900

    https://atcoder.jp/contests/abc242/tasks/abc242_c

--- AtCoder/ソース80.cpp
+++ AtCoder/ソース80.cpp
@@ -7,6 +7,19 @@
 //using namespace std;
 //typedef long long ll;
 //#define rep(i,n) for (int i = 0; i < (n); ++i)
+//ll n, sum;
+//void saiki(ll n, ll i) {
+//    if (n == 1) {
+//        ++sum;
+//        return;
+//    }
+//
+//    if (i != 1) saiki(n - 1, i - 1);
+//    if (i != 9) saiki(n - 1, i + 1);
+//    saiki(n - 1, i);
+//    
+//}
+//
 //
 //int main() {
 //    //sort(a.begin(), a.end());
@@ -14,9 +27,11 @@
 //        //map<int, vector<int>> mp;  ->  mp[a].push_back(i+1);
 //    //map<string, int> s; s[title] += 1; for (pair<string,int> val : s){} cnt = max(cnt, val.second);
 //    
-//    string s;
-//    cin >> s;
+//    cin >> n;
+//    
+//    for (ll i = 1; i < 10; ++i) {
+//        saiki(n, i);
+//    }
 //
-//    sort(s.begin(), s.end());
-//    cout << s << endl;
+//    cout << sum % 998244353 << endl;
 //}
