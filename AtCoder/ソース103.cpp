8ecbc2ad97e367f790f184856c06746bc1002e2e
DESKTOP-8EIQ5OC\nojsvt <nojsvt@gmail.com>
2022-05-09 12:47:39 +0900

    https://atcoder.jp/contests/abc250/tasks/abc250_b

--- AtCoder/ソース103.cpp
+++ AtCoder/ソース103.cpp
@@ -1,30 +1,30 @@
-#include <iostream>
-#include <vector>
-#include <string>
-#include <algorithm>
-#include <map>
-#include <set>
-using namespace std;
-typedef long long ll;
-typedef long long ll; const int inf = INT_MAX / 2; const ll infl = 1LL << 60;
-#define rep(i,n) for (int i = 0; i < (n); ++i)
-
-int main() {
-    //sort(a.begin(), a.end());
-    //vector<vector<int>> A(N, vector<int>(2));
-        //map<int, vector<int>> mp;  ->  mp[a].push_back(i+1);
-    //map<string, int> s; s[title] += 1; for (pair<string,int> val : s){} cnt = max(cnt, val.second);
-    int n, k;
-    cin >> n >> k;
-    
-    vector<int, map<char, int>> arr(n);
-    map<char, int> mp;
-    rep(i, n) {
-        string s;
-        cin >> s;
-        rep(j, s.size()) {
-            mp[s.at(j)] += 1;
-        }
-        
-    }
-}
+//#include <iostream>
+//#include <vector>
+//#include <string>
+//#include <algorithm>
+//#include <map>
+//#include <set>
+//using namespace std;
+//typedef long long ll;
+//typedef long long ll; const int inf = INT_MAX / 2; const ll infl = 1LL << 60;
+//#define rep(i,n) for (int i = 0; i < (n); ++i)
+//
+//int main() {
+//    int n, k;
+//    cin >> n >> k;
+//    vector<string> s(n);
+//    for (int i = 0; i < n; i++) cin >> s[i];
+//    int ans = 0;
+//    for (int i = 0; i < (1 << n); i++) {
+//        vector<int> sum(26);
+//        for (int j = 0; j < n; j++) {
+//            if ((i >> j) & 1) {
+//                for (int x = 0; x < s[j].size(); x++) sum[s[j][x] - 'a']++;
+//            }
+//        }
+//        int now = 0;
+//        for (int j = 0; j < 26; j++) if (sum[j] == k) now++;
+//        ans = max(ans, now);
+//    }
+//    cout << ans << endl;
+//}
