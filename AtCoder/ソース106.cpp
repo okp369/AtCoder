5808b112f583e797ab08ca2b6a3a94587cd87ce1
DESKTOP-8EIQ5OC\nojsvt <nojsvt@gmail.com>
2022-05-10 21:39:24 +0900

    https://atcoder.jp/contests/abc250/tasks/abc250_a

---
+++ AtCoder/ソース106.cpp
@@ -0,0 +1,48 @@
+//#include <iostream>
+//#include <vector>
+//#include <string>
+//#include <algorithm>
+//#include <map>
+//#include <set>
+//using namespace std;
+//typedef long long ll;
+//typedef long long ll; const int inf = INT_MAX / 2; const ll infl = 1LL << 60;
+//#define rep(i,n) for (int i = 0; i < (n); ++i)
+//
+//int main() {
+//    //sort(a.begin(), a.end());
+//    //vector<vector<int>> A(N, vector<int>(2));
+//        //map<int, vector<int>> mp;  ->  mp[a].push_back(i+1);
+//    //map<string, int> s; s[title] += 1; for (pair<string,int> val : s){} cnt = max(cnt, val.second);
+//    int n, a, b;
+//    cin >> n >> a >> b;
+//    
+//    rep(g, n) {
+//        rep(h, a) {
+//            rep(i, n) {
+//                rep(j, b) {
+//                    if (g % 2 == 0) {
+//                        if (i % 2 == 0) {
+//                            cout << ".";
+//                        }
+//                        else {
+//                            cout << "#";
+//                        }
+//                    }
+//                    else {
+//                        if (i % 2 == 0) {
+//                            cout << "#";
+//                        }
+//                        else {
+//                            cout << ".";
+//                        }
+//                    }                    
+//                }
+//            }
+//            cout << "" << endl;
+//        }
+//    }
+//    
+//    
+//    
+//}
